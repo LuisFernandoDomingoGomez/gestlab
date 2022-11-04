9d72e23bd77e0824a20f8acd97c1a6bba0bb778c
Luis Fernando Domingo Gómez <al222010066@gmail.com>
2022-11-04 14:30:12 -0600

    Users MVC

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -16,7 +16,7 @@
                         <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                             <li class="breadcrumb-item"><a href="{{ route('home') }}"><i class="fas fa-home"></i></a></li>
                             <li class="breadcrumb-item"><a href="{{ route('user.index') }}">Usuarios</a></li>
-                            <li class="breadcrumb-item"><a href="{{ route('user.index') }}">Gestion</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Gestión</li>
                         </ol>
                     </nav>
                 </div>
@@ -27,7 +27,6 @@
     <br>
     <div class="container-fluid mt--7">
         <div class="row">
-            <div class="row">
                 <div class="col-xl-3 col-lg-6">
                     <div class="card card-stats mb-4 mb-xl-0">
                         <div class="card-body">
@@ -54,7 +53,7 @@
                         <div class="card-body">
                             <div class="row">
                                 <div class="col">
-                                    <h5 class="card-title text-uppercase text-muted mb-0">Laboratoristas</h5>
+                                    <h5 class="card-title text-uppercase text-muted mb-0">Admins</h5>
                                     <span class="h2 font-weight-bold mb-0">8</span>
                                 </div>
                                 <div class="col-auto">
@@ -65,11 +64,12 @@
                             </div>
                             <p class="mt-3 mb-0 text-muted text-sm">
                                 <span class="text-success mr-2"><i class="fa fa-arrow-up"></i></span>
-                                <span class="text-nowrap">Total de Laboratoristas</span>
+                                <span class="text-nowrap">Total de Administradores</span>
                             </p>
                         </div>
                     </div>
                 </div>
+                <br><br><br><br><br><br><br>
                 <div class="col-xl-3 col-lg-6">
                     <div class="card card-stats mb-4 mb-xl-0">
                         <div class="card-body">
@@ -112,7 +112,6 @@
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
     </div>
     <div class="container-fluid">
@@ -125,10 +124,15 @@
                                 <h3 class="mb-0">Usuarios</h3>
                             </div>
                             <div class="col-4 text-right">
-                                <a href="" class="btn btn-sm btn-primary">Agregar</a>
+                                <a href="{{ route('users.create') }}" class="btn btn-sm btn-primary">Agregar</a>
                             </div>
                         </div>
                     </div>
+                    @if ($message = Session::get('success'))
+                        <div class="alert alert-success">
+                            <p>{{ $message }}</p>
+                        </div>
+                    @endif
                     
                     <div class="col-12">
                     </div>
@@ -139,6 +143,7 @@
                                 <tr>
                                     <th scope="col">Nombre</th>
                                     <th scope="col">Email</th>
+                                    <th scope="col">Rol</th>
                                     <th scope="col">Fecha de creación</th>
                                     <th scope="col"></th>
                                 </tr>
@@ -148,6 +153,13 @@
                                 <tr>
                                         <td>{{ $user->name }}</td>
                                         <td>{{ $user->email }}</td>
+                                        <td>
+                                            @if(!empty($user->getRoleNames()))
+                                                @foreach($user->getRoleNames() as $rolNombre)                                       
+                                                <h5><span class="badge badge-dark">{{ $rolNombre }}</span></h5>
+                                                @endforeach
+                                            @endif
+                                        </td>
                                         <td>{{ $user->created_at }}
                                         <td class="text-right">
                                             <div class="dropdown">
@@ -155,7 +167,11 @@
                                                     <i class="fas fa-ellipsis-v"></i>
                                                 </a>
                                                 <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                                                    <a class="dropdown-item" href="">Edit</a>
+                                                    <a class="dropdown-item" href="{{ route('users.edit',$user->id) }}">Edit</a>
+
+                                                    {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
+                                                        {!! Form::submit('Borrar', ['class' => 'dropdown-item']) !!}
+                                                    {!! Form::close() !!}
                                                 </div>
                                             </div>
                                         </td>
@@ -166,7 +182,7 @@
                     </div>
                     <div class="card-footer py-4">
                         <nav class="d-flex justify-content-end" aria-label="...">
-                            
+                            {!! $users->links() !!}
                         </nav>
                     </div>
                 </div>
